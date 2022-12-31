2f9dd236bf5cbc2f196bcc83a164f7d946e2b753
D3vanagar1 <755980@gmail.com>
2022-12-30 21:30:36 -0800

    reformatting

--- lua/D3vanagar1/plugins/luasnip.lua
+++ lua/D3vanagar1/plugins/luasnip.lua
@@ -1,11 +1,11 @@
 -- press <Tab> to expand or jump in a snippet. These can also be mapped separately
 -- via <Plug>luasnip-expand-snippet and <Plug>luasnip-jump-next.
 -- -1 for jumping backwards.
-vim.keymap.set("i", "<silent> <S-Tab>", require('luasnip').jump(-1))
+vim.keymap.set("i", "<silent> <S-Tab>", require("luasnip").jump(-1))
 
-vim.keymap.set("s", "<silent> <Tab>", require('luasnip').jump(1))
-vim.keymap.set("s", "<silent> <S-Tab>", require('luasnip').jump(-1))
+vim.keymap.set("s", "<silent> <Tab>", require("luasnip").jump(1))
+vim.keymap.set("s", "<silent> <S-Tab>", require("luasnip").jump(-1))
 
 -- For changing choices in choiceNodes (not strictly necessary for a basic setup).
-imap <silent><expr> <C-E> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>'
-smap <silent><expr> <C-E> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>'
+-- imap <silent><expr> <C-E> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>'
+-- smap <silent><expr> <C-E> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>'
