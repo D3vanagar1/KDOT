1ec7d15896355c7de00cc9af56e7856416ea647c
D3vanagar1 <755980@gmail.com>
2023-01-12 15:08:30 -0800

    fix: luasnip works. able to jump between snippet items with C-l and C-g

--- lua/D3vanagar1/plugins/luasnip.lua
+++ lua/D3vanagar1/plugins/luasnip.lua
@@ -1,11 +1,13 @@
--- press <Tab> to expand or jump in a snippet. These can also be mapped separately
--- via <Plug>luasnip-expand-snippet and <Plug>luasnip-jump-next.
--- -1 for jumping backwards.
-vim.keymap.set("i", "<silent> <S-Tab>", require("luasnip").jump(-1))
+local ls = require("luasnip")
+-- expand current item or jump to next item within snippet
+vim.keymap.set({ "i", "s" }, "<C-l>", function()
+	if ls.expand_or_jumpable() then
+		ls.expand_or_jump()
+	end
+end, { silent = true })
 
-vim.keymap.set("s", "<silent> <Tab>", require("luasnip").jump(1))
-vim.keymap.set("s", "<silent> <S-Tab>", require("luasnip").jump(-1))
-
--- For changing choices in choiceNodes (not strictly necessary for a basic setup).
--- imap <silent><expr> <C-E> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>'
--- smap <silent><expr> <C-E> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>'
+vim.keymap.set({ "i", "s" }, "<C-g>", function()
+	if ls.jumpable(-1) then
+		ls.jump(-1)
+	end
+end, { silent = true })
