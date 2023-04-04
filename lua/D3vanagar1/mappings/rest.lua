40d0de2c7a023a8c23356592be20e89229ca1aac
D3vanagar1 <755980@gmail.com>
2023-04-03 19:40:52 -0700

    FIxed moving text bug no longer pastes unnecesasry ':' when moving text in insert mode

--- lua/D3vanagar1/mappings/rest.lua
+++ lua/D3vanagar1/mappings/rest.lua
@@ -10,7 +10,7 @@ vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<c-u>", "<c-u>zz")
 
 -- Set default search to be "very magic" mode for reg exprs
 -- TODO: doesn't work in lua
@@ -20,8 +20,8 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 -- Moving text (move text up/down with k/j)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-vim.keymap.set("i", "<C-j>", ":<esc>:m .+1<CR>==i")
-vim.keymap.set("i", "<C-k>", ":<esc>:m .-2<CR>==i")
+vim.keymap.set("i", "<C-j>", "<esc>:m .+1<CR>==i")
+vim.keymap.set("i", "<C-k>", "<esc>:m .-2<CR>==i")
 vim.keymap.set("n", "<Leader>j", ":m .+1<CR>==")
 vim.keymap.set("n", "<Leader>k", ":m .-2<CR>==")
 
