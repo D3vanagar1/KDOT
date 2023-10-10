def6db53e3467aa72eb4e8e1fdbe1cdf42221106
D3vanagar1 <755980@gmail.com>
2023-10-09 23:26:12 -0700

    modified harpoon keybindings these keybindins make more intuitive sense being on the home row. Each finger indicates a file. Used mac code for each alt+key so that nvim would recognize it

--- lua/D3vanagar1/plugins/harpoon.lua
+++ lua/D3vanagar1/plugins/harpoon.lua
@@ -2,17 +2,18 @@ local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
 vim.keymap.set("n", "<Leader>a", mark.add_file)
-vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+vim.keymap.set("n", "©", ui.toggle_quick_menu) -- © is the mac key code for alt+g
 
-vim.keymap.set("n", "<C-p>", function()
-	ui.nav_file(1)
-end)
-vim.keymap.set("n", "<C-b>", function()
+vim.keymap.set("n", "˙", function() -- ˙ is the mac key code for alt+h
 	ui.nav_file(2)
 end)
-vim.keymap.set("n", "<C-n>", function()
+
+vim.keymap.set("n", "∆", function() -- ∆ is the mac key code for alt+j
+	ui.nav_file(2)
+end)
+vim.keymap.set("n", "˚", function() -- ˚ is the mac key code for alt+k
 	ui.nav_file(3)
 end)
-vim.keymap.set("n", "<C-m>", function()
+vim.keymap.set("n", "¬", function() -- ¬ is the mac key code for alt+l
 	ui.nav_file(4)
 end)
