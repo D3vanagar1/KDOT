138a00bbc46618114dabae54ffd2f254687bc199
D3vanagar1 <755980@gmail.com>
2023-10-18 01:46:23 -0700

    netrw settings and format on save
    added some basic netrw settings to remove annoying banner and follow
    tree structure
    
    brought back format on save. What to see how it feels

--- lua/D3vanagar1/settings/netrw.lua
+++ lua/D3vanagar1/settings/netrw.lua
@@ -1,6 +1,8 @@
 -- Netrw configuration
 
 vim.g.netrw_keepdir = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_liststyle = 3
 vim.g.netrw_localcopydircmd = "cp -r"
 
 -- Open Netrw of current directory in a "Project Drawer" style on the right side
