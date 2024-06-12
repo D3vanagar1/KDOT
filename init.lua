92d50febdc13489f444e68c373dea3e365c539e9
D3vanagar1 <755980@gmail.com>
2024-06-12 15:09:38 -0700

    added set of language plugins
    these languages aren't normally supported by nvim-lsp
    so needed external configuration

--- init.lua
+++ init.lua
@@ -29,16 +29,6 @@ vim.opt.rtp:prepend(lazypath)
 -- [[ Basic autocommands]]
 require 'D3vanagar1.autocmds'
 
--- [[ Configure and install plugins ]]
---
---  To check the current status of your plugins, run
---    :Lazy
---
---  You can press `?` in this menu for help. Use `:q` to close the window
---
---  To update plugins you can run
---    :Lazy update
---
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
---
+++ lua/D3vanagar1/plugins/langauges.lua
@@ -0,0 +1,11 @@
+return {
+  -- Vim support for mathematica
+  'voldikss/vim-mma',
+  -- Fennel syntax highlight
+  'mnacamura/vim-fennel-syntax',
+  -- Solidity syntax highlight
+  'thesis/vim-solidity',
+  -- vim mmarkdown
+  'godlygeek/tabular',
+  'preservim/vim-markdown',
+}
